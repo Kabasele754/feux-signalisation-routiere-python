a8eeb91bfe3dfb72d54af929868e30fd6f92d54d
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2024-03-21 16:40:02 +0200

    Update base_feux_de_signalisation.py

--- base_feux_de_signalisation.py
+++ base_feux_de_signalisation.py
@@ -1,1 +1,24 @@
+import time
+
+# Définition des durées pour chaque état des feux
+DUREE_VERTE = 5
+DUREE_ORANGE = 2
+DUREE_ROUGE = 5
+
+# Initialisation de l'état initial des feux
+etat_feux = "vert"
+
+while True:
+    if etat_feux == "vert":
+        print("Feu vert - Passage autorisé")
+        time.sleep(DUREE_VERTE)
+        etat_feux = "orange"
+    elif etat_feux == "orange":
+        print("Feu orange - Attention, le feu va passer au rouge")
+        time.sleep(DUREE_ORANGE)
+        etat_feux = "rouge"
+    elif etat_feux == "rouge":
+        print("Feu rouge - Arrêt obligatoire")
+        time.sleep(DUREE_ROUGE)
+        etat_feux = "vert"
 
